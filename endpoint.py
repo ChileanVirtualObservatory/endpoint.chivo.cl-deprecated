3e539dea38ce9b83e3f1061a3a081947cec09ce9
jantogni <jantogni@csrg.cl>
2013-09-27 11:10:02 -0300

    adding changes to endpoint (scs, ssa, sia) and tsQuery test

--- endpoint.py
+++ endpoint.py
@@ -1,12 +1,14 @@
 import subprocess
 import urllib
 import urllib2
+import requests
 from os import system
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
 SERVER_TAP = 'http://localhost:8080/cadcSampleTAP/sync'
-
+SERVER_SCS = 'http://wfaudata.roe.ac.uk/twomass-dsa/DirectCone?DSACAT=TWOMASS&DSATAB=twomass_psc'
+SERVER_SSA = 'http://wfaudata.roe.ac.uk/6dF-ssap/?'
 
 @app.route('/')
 def index():
@@ -43,10 +45,20 @@ def chivo_query(query):
 		if request.method == 'GET':
 			#Argumentos consulta metodo GET 
 			#TODO: nombre parametros
-			RA = request.args.get('RA')
-			DEC = request.args.get('DEC')
+			RA = request.args.get('ra')
+			DEC = request.args.get('dec')
 			RADIUS = request.args.get('RADIUS')
-	 
+	        
+			print RA
+			print DEC
+			print RADIUS
+
+			#ejemplo
+			#get = http://wfaudata.roe.ac.uk/twomass-dsa/DirectCone\?DSACAT\=TWOMASS\&DSATAB\=twomass_psc\&ra\=21\&dec\=15\&RADIUS\=1
+			values = {'ra' : RA, 'dec' : DEC, 'RADIUS' : RADIUS}
+
+			r = requests.get(SERVER_SCS, params=values)
+ 
 			#Validar argumentos 
 			#TODO: revisar nombres de los parametros y si son case sensitive
 	
@@ -54,7 +66,9 @@ def chivo_query(query):
 			#TODO: buscar un servicio que soporte SCS para probar mientras y ejecutar el request
 	
 			#Retornar respuesta servidor TAP %TODO
-			return 'Under construction'
+			
+			return r.content
+			#return 'SCSP Under construction'
 		
 		return 'Bad Request'
 
@@ -70,13 +84,18 @@ def chivo_query(query):
 			#TODO: buscar un servicio que soporte SIA para probar mientras y ejecutar el request
 	
 			#Retornar respuesta servidor TAP %TODO
-			return 'Under construction'
+			return 'SIAP Under construction'
 		
 		return 'Bad Request'
 
 
 	if query == 'ssa':
 		if request.method == 'GET':
+			values = {'POS' : '22.438,-17.2',
+			          'SIZE' : '0.02'}
+
+			r = requests.get(SERVER_SSA, params=values)
+
 			#Argumentos consulta metodo GET 
 			#TODO: revisar parametros de busqueda y si es que solo soporta GET
 	
@@ -87,7 +106,7 @@ def chivo_query(query):
 			#TODO: buscar un servicio que soporte SSA para probar mientras y ejecutar el request
 	
 			#Retornar respuesta servidor TAP %TODO
-			return 'Under construction'
+			return r.content
 		
 		return 'Bad Request'
 
