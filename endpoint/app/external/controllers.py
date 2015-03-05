e49da71a421008ab093176249c911a3c52353a35
Camilo Valenzuela <cacke91@gmail.com>
2015-03-05 15:25:49 -0300

    Rollback to old external

--- endpoint/app/external/controllers.py
+++ endpoint/app/external/controllers.py
@@ -2,8 +2,7 @@
 from flask import Blueprint, request, render_template, \
                   flash, g, session, redirect, url_for, Response
 
-from werkzeug.contrib.cache import SimpleCache
-
+from app.external.models import ExtService, ExtRegistry, VOparisRegistry
 from app.services.models import ChivoRegistry
 from app.helpers.functions import *
 
@@ -11,53 +10,19 @@ import requests
 import json
 
 #Creating objects
+extReg = VOparisRegistry() 
 chivoReg = ChivoRegistry()
 
 # Define the blueprint: 'services'
 external = Blueprint('external', __name__)
 
-#Defining constants
-SERVICEPARAMS = {
-		"tap": "ivo://ivoa.net/std/TAP",
-		"sia": "ivo://ivoa.net/std/SIA",
-		"ssa": "ivo://ivoa.net/std/SSA",
-		"scs": "ivo://ivoa.net/std/ConeSearch",
-		}
-		
-#Max response items
-MAX = 1000
-
-#Cache
-cache = SimpleCache()
-
-#VoParis Registry
-def registry(service= "tap"):
-
-	catalogsList = cache.get(service)
-	if catalogsList is None:
-		standardid = 'standardid:"'+ SERVICEPARAMS[service] + '"'
-		parameters ={"keywords": standardid, "max": MAX}
-		r = requests.get( "http://voparis-registry.obspm.fr/vo/ivoa/1/voresources/search", params = parameters)
-		entries = json.loads(r.content)['resources']
-	
-		catalogsList = []
-		for entry in entries:
-			if entry["status"] == "active":
-				try:
-					dic = {}
-					dic["title"] = entry["title"]
-					dic["shortname"] = entry["shortname"]
-					for s in entry["capabilities"]:
-						if s["standardid"] == SERVICEPARAMS[service]:
-							dic["accessurl"] = s["accessurl"]
-					catalogsList.append(dic)
-				except:
-					pass
-		cache.set(service, catalogsList, timeout= 24*60*60)
-	return json.dumps(catalogsList)
-
 def registry1(Reg = chivoReg,  service = "tap"):
-
+	SERVICEPARAMS = {
+			"tap": "ivo://ivoa.net/std/TAP",
+			"sia": "ivo://ivoa.net/std/SIA",
+			"ssa": "ivo://ivoa.net/std/SSA",
+			"scs": "ivo://ivoa.net/std/ConeSearch",
+			}
 	cat = []
 	for i in Reg.catalogs.keys():
 		cati=Reg.getCatalog(i)
@@ -81,7 +46,7 @@ def registry1(Reg = chivoReg,  service = "tap"):
 @external.route('/external/tap', methods = ['GET'])
 def extRegistry1():
 	internal = json.loads(registry1(chivoReg, "tap"))
-	external = json.loads(registry("tap"))
+	external = json.loads(registry1(extReg ,"tap"))
 	return json.dumps(internal+external)
 	
 	
@@ -89,19 +54,25 @@ def extRegistry1():
 @external.route('/external/scs', methods = ['GET'])
 def extRegistry2():
 	internal = json.loads(registry1(chivoReg, "scs"))
-	external = json.loads(registry("scs"))
+	external = json.loads(registry1(extReg ,"scs"))
 	return json.dumps(internal+external)
 
 @external.route('/external/registry/allSia', methods = ['GET'])
 @external.route('/external/sia', methods = ['GET'])
 def extRegistry3():
 	internal = json.loads(registry1(chivoReg, "sia"))
-	external = json.loads(registry("sia"))
+	external = json.loads(registry1(extReg ,"sia"))
 	return json.dumps(internal+external)
 	
 @external.route('/external/registry/allSsa', methods = ['GET'])
 @external.route('/external/ssa', methods = ['GET'])
 def extRegistry4():
-	external = json.loads(registry("ssa"))
+	external = json.loads(registry1(extReg ,"ssa"))
 	return json.dumps(external)
 	
+#External Registry
+@external.route('/external/registry/', methods = ['GET'])
+def extRegistry():
+	internal = json.loads(registry(chivoReg))
+	external = json.loads(registry(extReg))
+	return json.dumps(internal+external)
