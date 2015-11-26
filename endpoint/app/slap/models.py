dc30554f04d1af2e55d751f0544ce1aee61b2e2f
Teodoro Hochfärber Salinas <teohoch2@gmail.com>
2015-11-26 00:17:51 -0300

    Completely developed the generic input processing, and added an almost complete set of test to it (should add error case tests).  Should also include some code to only allow whitelisted input parameters

--- endpoint/app/slap/models.py
+++ endpoint/app/slap/models.py
@@ -5,22 +5,23 @@ support_mapping = "support"
 
 import elasticsearch
 from multiprocessing import cpu_count
-from multiprocess import Pool # Allows for efficient parallel Map, To install use pip install multiprocess
+from multiprocess import Pool  # Allows for efficient parallel Map, To install use pip install multiprocess
 
 numeric_fields = ["WAVELENGHT", "FREQUENCY"]
+
 
 class ElasticQuery():
     '''
     Class designed to retrieve easily SLAP data from Elasticsearch
     '''
 
-    def __init__(self,host, primary_index, primary_mapping, support_mapping):
+    def __init__(self, host, primary_index, primary_mapping, support_mapping):
         self.__host = host
         self.__primary_index = primary_index.lower()
         self.__primary_mapping = primary_mapping
         self.__support_mapping = support_mapping
         self.__connection = elasticsearch.Elasticsearch(hosts=self.__host)
-        self.__query_dic = {"query":{}}
+        self.__query_dic = {"query": {}}
         self.__max_result_size = self.__connection.count()["count"]
         self.__numeric_fields = numeric_fields
 
@@ -36,11 +37,11 @@ class ElasticQuery():
         else:
             raise ValueError("You must specify either a maximum or minimum")
 
-    def add_wavelenght_to_query(self, minimum = None, maximum = None):
+    def add_wavelenght_to_query(self, minimum=None, maximum=None):
         if not self.__query_dic["query"].has_key("range"):
             self.__query_dic["query"]["range"] = {}
         if minimum and maximum:
-            self.__query_dic["query"]["range"]["Wavelenght"] = {"gte" : minimum, "lte": maximum}
+            self.__query_dic["query"]["range"]["Wavelenght"] = {"gte": minimum, "lte": maximum}
         elif maximum:
             self.__query_dic["query"]["range"]["Wavelenght"] = {"lte": maximum}
         elif minimum:
@@ -51,10 +52,9 @@ class ElasticQuery():
     def __extractor(self, element):
         return element["_source"].values()
 
-
-
-    def send_query(self,timeout=180):
-        data = self.__connection.search(index=self.__primary_index, doc_type=self.__primary_mapping, body=self.__query_dic, size=self.__max_result_size,request_timeout=timeout)
+    def send_query(self, timeout=180):
+        data = self.__connection.search(index=self.__primary_index, doc_type=self.__primary_mapping,
+                                        body=self.__query_dic, size=self.__max_result_size, request_timeout=timeout)
         query_size = data["hits"]["total"]
         query_time = data["took"]
         query_data = data["hits"]["hits"]
@@ -66,8 +66,8 @@ class ElasticQuery():
         data.clear()
         del query_data[:]
 
-        #filtered_data = map(self.__extractor,query_data)
-        return {"results":filtered_data,"time":query_time,"total":query_size}
+        # filtered_data = map(self.__extractor,query_data)
+        return {"results": filtered_data, "time": query_time, "total": query_size}
 
     def range(self, limits, param):
         q = {}
@@ -80,29 +80,64 @@ class ElasticQuery():
         else:
             raise ValueError("You must specify either a maximum or minimum")
 
-        return { "range" : { param : q }}
+        return {"range": {param: q}}
 
     def equality(self, value, param):
-	    processed_value = value
-	    if isinstance(value, list):
-		    if param.upper() in numeric_fields:
-			    processed_value = map(lambda x:float(x),value)
-	    else:
-		    if param.upper() in numeric_fields:
-			    value = float(value)
-	    return {"term": {param: processed_value}}
+        processed_value = value
+        query_type = "match"
+        if param.upper() in numeric_fields:
+            processed_value = float(value)
+            query_type = "term"
+            # This will match EXACTLY the number.
+            # I.E. if value is 16.3, will only match 16.30, 16.3000, 16.3000000 and so on, but not 16.30000001
+
+        return {query_type: {param: processed_value}}
+
+    def constrain_parser(self, value, param):
+        splited_constrains = value.split(",")
+        processed_constrains = []
+
+        for con_value in splited_constrains:
+            v = con_value.split("/")
+            if len(v) == 2:
+                 processed_constrains.append(self.range(v, param))
+            elif len(v) == 1:
+                processed_constrains.append(self.equality(v[0], param))
+            else:
+                raise Exception
+
+        output = {
+            "bool":
+                {
+                    "should": processed_constrains,
+                    "minimum_should_match": 1
+                }
+        }
+        return output
+
+
+
 
     def parser(self, query):
+        processed_conditions = []
         for key, value in query.items():
-            contrains = value.split(",")
-            for con in contrains:
-                v = con.split("/")
-                if len(v)==2:
-                    con_value = self.range(v,key)
-                elif len(v)==1:
-                    con_value = self.equality(v,key)
-                else:
-                    raise Exception
+            output = self.constrain_parser(value, key)
+            processed_conditions.append(output)
+
+        output = {
+            "query":
+                {
+                    "bool":{
+                        "must": processed_conditions
+                    }
+                }
+        }
+        return output
+
+
+
+
+
 
 
 
--- endpoint/app/slap/test_elasticQuery.py
+++ endpoint/app/slap/test_elasticQuery.py
@@ -1,19 +1,18 @@
 from unittest import TestCase
 from models import ElasticQuery
 
-
 class TestElasticQuery(TestCase):
 
 	def setUp(self):
-		self.query = ElasticQuery("http://otto.csrg.cl:9200", "sl-repository", "line","support")
-
+		self.query = ElasticQuery("http://otto.csrg.cl:9200", "sl-repository", "line", "support")
+		self.maxDiff = None
 
 	def test_range_upper_and_lower(self):
-		expected_output = { "range":{"Freq": {"gte": 1.0,"lte": 1.2}}}
+		expected_output = {"range": {"Freq": {"gte": 1.0, "lte": 1.2}}}
 		term = "Freq"
 		params = ["1", "1.2"]
-		output = self.query.range(params,term)
-		self.assertEquals(expected_output,output)
+		output = self.query.range(params, term)
+		self.assertEquals(expected_output, output)
 
 	def test_range_upper(self):
 		expected_output = {"range": {"Freq": {"lte": 1.2}}}
@@ -29,61 +28,448 @@ class TestElasticQuery(TestCase):
 		output = self.query.range(params, term)
 		self.assertEquals(expected_output, output)
 
-
-
-	def test_equality_numeric_single(self):
+	def test_equality_numeric(self):
 		term = "Frequency"
 		param = "1"
-		expected_output = {"term": { "Frequency": 1.0}}
-		output = self.query.equality(param,term)
-		self.assertEquals(output,expected_output)
-
-	def test_equality_numeric_multiple(self):
-		term = "Frequency"
-		param = ["1","1.3","6.34"]
-		expected_output = {"term": {"Frequency": [1.0, 1.3, 6.34]}}
+		expected_output = {"term": {"Frequency": 1.0}}
 		output = self.query.equality(param, term)
 		self.assertEquals(output, expected_output)
 
-	def test_equality_string_single(self):
+	def test_equality_string(self):
 		term = "molecular_name"
 		param = "acid"
-		expected_output = {"term": {"molecular_name": "acid"}}
+		expected_output = {"match": {"molecular_name": "acid"}}
 		output = self.query.equality(param, term)
 		self.assertEquals(output, expected_output)
 
-	def test_equality_string_multiple(self):
-		term = "molecular_name"
-		param = ["acid", "oxygen", "hydrogen"]
-		expected_output = {"term": {"molecular_name": ["acid", "oxygen", "hydrogen"]}}
-		output = self.query.equality(param, term)
-		self.assertEquals(output, expected_output)
+	def test_constrain_parser_list_equality_numeric(self):
+		term = "Frequency"
+		values = "1.2,2.5,3"
+		input = {term : values}
+		expected_output = {
+			"bool":
+				{
+					"should":
+						[
+							{
+								"term":
+									{
+										"Frequency": 1.2
+									}
+							},
+							{
+								"term": {
+									"Frequency": 2.5
+								}
+							},
+							{
+								"term": {
+									"Frequency": 3
+								}
+							}
+						],
+					"minimum_should_match": 1
+				}
+		}
 
-	def test_parser_equality_numeric_single(self):
-		self.fail()
+		output = self.query.constrain_parser(values,term)
+		self.assertEqual(output,expected_output)
 
-	def test_parser_equality_numeric_multiple(self):
-		self.fail()
+	def test_constrain_parser_list_range_numeric(self):
+		term = "Frequency"
+		values = "1.2/2.5,3/5,6/7"
+		input = {term : values}
+		expected_output = {
+			"bool":
+				{
+					"should":
+						[
+							{
+								"range":
+									{
+										"Frequency":
+											{
+												"gte": 1.2,
+												"lte": 2.5
+											}
+									}
+							},
+							{
+								"range":
+									{
+										"Frequency":
+											{
+												"gte": 3.0,
+												"lte": 5.0
+											}
+									}
+							},
+							{
+								"range":
+									{
+										"Frequency":
+											{
+												"gte": 6,
+												"lte": 7
+											}
+									}
+							}
+						],
+					"minimum_should_match": 1
+				}
+		}
 
-	def test_parser_equality_string_single(self):
-		self.fail()
+		output = self.query.constrain_parser(values,term)
+		self.assertEqual(output,expected_output)
 
-	def test_parser_equality_string_multiple(self):
-		self.fail()
+	def test_constrain_parser_list_mix_numeric(self):
+		term = "Frequency"
+		values = "1.2/2.5,3/5,6/7,1.2,2.5,3"
+		input = {term : values}
+		expected_output = {
+			"bool":
+				{
+					"should":
+						[
+							{
+								"range":
+									{
+										"Frequency":
+											{
+												"gte": 1.2,
+												"lte": 2.5
+											}
+									}
+							},
+							{
+								"range":
+									{
+										"Frequency":
+											{
+												"gte": 3.0,
+												"lte": 5.0
+											}
+									}
+							},
+							{
+								"range":
+									{
+										"Frequency":
+											{
+												"gte": 6,
+												"lte": 7
+											}
+									}
+							},
+							{
+								"term":
+									{
+										"Frequency": 1.2
+									}
+							},
+							{
+								"term": {
+									"Frequency": 2.5
+								}
+							},
+							{
+								"term": {
+									"Frequency": 3
+								}
+							}
+						],
+					"minimum_should_match": 1
+				}
+		}
 
-	def test_parser_range_upper_and_lower(self):
-		self.fail()
+		output = self.query.constrain_parser(values,term)
+		self.assertEqual(output,expected_output)
 
-	def test_parser_range_upper(self):
-		self.fail()
+	def test_constrain_parser_list_equality_string(self):
+		term = "Chemical Name"
+		values = "Formic Acid,Formyl Chloride"
 
-	def test_parser_range_lower(self):
-		self.fail()
+		expected_output = {
+			"bool":
+				{
+					"should":
+						[
+							{"match": {"Chemical Name": "Formic Acid"}},
+							{"match": {"Chemical Name": "Formyl Chloride"}}
+						],
+					"minimum_should_match": 1
+				}
+		}
 
-	def test_parser(self):
-		self.fail()
+		output = self.query.constrain_parser(values,term)
+		self.assertEqual(output,expected_output)
 
+	def test_parser_single_equality(self):
+		input = {
+			"Chemical Name": "Formic Acid,Formyl Chloride"
+		}
+		expected_output = {
+			"query":
+				{
+					"bool":
+						{
+							"must":
+								[
+									{
+										"bool":
+											{
+												"should":
+													[
+														{"match": {"Chemical Name": "Formic Acid"}},
+														{"match": {"Chemical Name": "Formyl Chloride"}}
+													],
+												"minimum_should_match": 1
+											}
+									}
+								]
+						}
+				}
+		}
+		output = self.query.parser(input)
+		self.assertEqual(output,expected_output)
 
+	def test_parser_multiple_all_equality(self):
+		input = {
+			"Chemical Name": "Formic Acid,Formyl Chloride",
+			"Frequency": "1.2,2.5,3"
+		}
+		expected_output = {
+			"query":
+				{
+					"bool":
+						{
+							"must":
+								[
+									{
+										"bool":
+											{
+												"should":
+													[
+														{
+															"term":
+																{
+																	"Frequency": 1.2
+																}
+														},
+														{
+															"term": {
+																"Frequency": 2.5
+															}
+														},
+														{
+															"term": {
+																"Frequency": 3.0
+															}
+														}
+													],
+												"minimum_should_match": 1
+											}
+									},
+									{
+										"bool":
+											{
+												"should":
+													[
+														{"match": {"Chemical Name": "Formic Acid"}},
+														{"match": {"Chemical Name": "Formyl Chloride"}}
+													],
+												"minimum_should_match": 1
+											}
+									}
 
+								]
+						}
+				}
+		}
+		output = self.query.parser(input)
 
+		self.assertEqual(output,expected_output)
 
+	def test_parser_single_range(self):
+		input = {
+			"Frequency": "1.2/2.5,3/5,6/7"
+		}
+		expected_output = {
+			"query":
+				{
+					"bool":
+						{
+							"must":
+								[
+									{
+										"bool":
+											{
+												"should":
+													[
+														{
+															"range":
+																{
+																	"Frequency":
+																		{
+																			"gte": 1.2,
+																			"lte": 2.5
+																		}
+																}
+														},
+														{
+															"range":
+																{
+																	"Frequency":
+																		{
+																			"gte": 3.0,
+																			"lte": 5.0
+																		}
+																}
+														},
+														{
+															"range":
+																{
+																	"Frequency":
+																		{
+																			"gte": 6,
+																			"lte": 7
+																		}
+																}
+														}
+													],
+												"minimum_should_match": 1
+											}
+									}
+								]
+						}
+				}
+		}
+		output = self.query.parser(input)
+
+		self.assertEqual(output,expected_output)
+
+	def test_parser_multiple_range_equality(self):
+		input = {
+			"Frequency": "1584800/1584900,1585000/1585100",
+			"Chemical Name": "Formic Acid,Formyl Chloride"
+		}
+		expected_output = {
+			"query": {
+				"bool": {
+					"must": [
+						{
+							"bool": {
+								"should":
+									[
+										{
+											"range":
+												{
+													"Frequency":
+														{
+															"gte": 1584800,
+															"lte": 1584900
+														}
+												}
+										},
+										{
+											"range":
+												{
+													"Frequency": {
+														"gte": 1585000,
+														"lte": 1585100
+												}
+											}
+										}
+									],
+								"minimum_should_match": 1
+							}
+						},
+						{
+							"bool": {
+								"should": [
+									{
+										"match": {
+											"Chemical Name": "Formic Acid"
+										}
+									},
+									{
+										"match": {
+											"Chemical Name": "Formyl Chloride"
+										}
+									}
+								],
+								"minimum_should_match": 1
+							}
+						}
+					]
+				}
+			}
+		}
+		output = self.query.parser(input)
+
+		self.assertEqual(output,expected_output)
+
+	def test_parser_multiple_mix(self):
+		input = {
+			"Frequency": "1584800/1584900,1585000/1585100,1529943.39901",
+			"Chemical Name": "Formic Acid,Formyl Chloride"
+		}
+		expected_output = {
+			"query": {
+				"bool": {
+					"must": [
+						{
+							"bool": {
+								"should":
+									[
+										{
+											"range":
+												{
+													"Frequency":
+														{
+															"gte": 1584800,
+															"lte": 1584900
+														}
+												}
+										},
+										{
+											"range": {
+												"Frequency": {
+													"gte": 1585000,
+													"lte": 1585100
+												}
+											}
+										},
+										{
+											"term": {
+												"Frequency": 1529943.39901
+											}
+										}
+									],
+								"minimum_should_match": 1
+							}
+						},
+						{
+							"bool": {
+								"should": [
+									{
+										"match": {
+											"Chemical Name": "Formic Acid"
+										}
+									},
+									{
+										"match": {
+											"Chemical Name": "Formyl Chloride"
+										}
+									}
+								],
+								"minimum_should_match": 1
+							}
+						}
+					]
+				}
+			}
+		}
+		output = self.query.parser(input)
+
+		self.assertEqual(output,expected_output)
